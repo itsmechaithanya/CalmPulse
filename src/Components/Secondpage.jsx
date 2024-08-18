30cdf3c76d51b326ab3b5a6c442b00fb150ed775
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-08-18 12:10:18 +0530

    Your commit message

--- src/Components/Secondpage.jsx
+++ src/Components/Secondpage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import imgg from '../assets/b08f403ff43bbe883c702ab13eccb016.png'
 import imggg from '../assets/247ade28c862c6b10881b9307e6df568.png'
+import { FcGoogle } from "react-icons/fc";
+import { IconContext } from "react-icons";
 
-function SignUpPage() {
+function Secondpage() {
   return (
     <div className='h-screen w-screen bg-[#7A4BC8] overflow-hidden relative'>
         <img className='h-[20vh] absolute -top-[5vh] -right-2' src={imggg} alt="" />
         <h1 className='text-white text-3xl font-semibold flex justify-center pt-[10vh]'>Sign Up</h1>
-        <div className='flex justify-center mt-[3vh]'>
-            <button className='text-white bg-black px-10 py-3 rounded-full'>Sign up with Google</button>
+        <div className='flex flex-col justify-center mt-[3vh] items-center'>
+            <button className='text-white bg-black px-8 flex justify-center items-center py-5 rounded-full gap-3'><FcGoogle size={"1.5em"} />Sign up with Google</button>
         </div>
-        <h1>Con</h1>
+        <div className='flex justify-center mt-2'>
+            <h1 className=' capitalize'>Or continue with Email</h1>
+        </div>
         <img className='absolute -bottom-[30vh] -left-[11vh] h-[60vh]' src={imgg} alt="" />
+            
     </div>
   )
 }
