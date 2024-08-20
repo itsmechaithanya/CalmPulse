288ea265de284dd949d5f971a4d214493dc561f4
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-08-20 18:07:36 +0530

    Your commit message

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import imggg from '../assets/247ade28c862c6b10881b9307e6df568.png'
 
 function Details() {
   return (
-    <div>
+    <div className='h-screen w-screen bg-[#7A4BC8]'>
+        <img className='h-[20vh] absolute -top-[5vh] -right-2' src={imggg} alt="" />
+        <div className='flex justify-center pt-[10vh]'>
+            <h1 className='text-white text-[3vh] font-semibold'>Users Details Form</h1>
+        </div>
         
     </div>
   )
